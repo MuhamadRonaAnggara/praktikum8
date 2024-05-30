4c07105ad1e21d353768897e6bbd408882a87d44
unknown <anggara@students.amikom.ac.id>
2024-05-30 14:55:01 +0700

    vue js

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Ini adalah Banteng Merah</h1>
+        <p>SOLIIDDDDDDD!!!!!!!!!!!!!!</p>
+    </div>
+    <img src="../assets/pdi.jpeg" alt="" width="400px">
+</template>
